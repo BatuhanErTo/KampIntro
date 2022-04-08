113f72f95239a7db69986428323e88b35bba384c
Batuhan Erol <bataer57@gmail.com>
2022-04-09 00:00:49 +0300

    Added | OOP1

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        /**
+        public void Change(Product product)
+        {
+            product.ProductName = "Avize";
+        }
+        **/
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "added.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "updated.");
+        }
+        
+        
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id = 2, CategoryId = 5, 
+                UnitsInStock = 5, ProductName = "Pencil", UnitPrice = 35};
+            
+            ProductManager productManager = new ProductManager();
+            /**
+            productManager.Change(product2);
+            Console.WriteLine(product2.ProductName);// class and interface are reference type 
+            **/
+
+
+        }
+    }
+}

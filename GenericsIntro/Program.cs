c18d47f93558303e266fc31a46a26ff73520b3e5
Batuhan Erol <bataer57@gmail.com>
2022-04-10 02:49:21 +0300

    Added | GenericsIntro

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsIntro // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("h");
+        }
+    }
+}

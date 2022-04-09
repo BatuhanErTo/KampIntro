1d1acd419d575f591fc66fb9e8af3f893bd73054
Batuhan Erol <bataer57@gmail.com>
2022-04-10 02:48:48 +0300

    Added | Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] str = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(str[0]);
+            //Console.WriteLine(str[1]);
+            //Console.WriteLine(str[2]);
+            //Console.WriteLine(str[3]);
+            //str = new string[5];
+            //str[4] = "FFFF";
+            //Console.WriteLine(str[4]);
+            //Console.WriteLine(str[0]);
+            //Console.WriteLine(str[1]);
+            //Console.WriteLine(str[2]);
+            //Console.WriteLine(str[3]);
+
+            List<string> isimler2 = new List<string>() {"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}

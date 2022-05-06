d38c2fa6c5161e8c0bc50a3f63ae06751d0a88a2
Batuhan Erol <bataer57@gmail.com>
2022-05-06 17:20:59 +0300

    Added | MultipleImplementation

---
+++ MultipleImplementation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultipleImplementation // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IWorker> workers = new List<IWorker>() { new Robot(), new Worker(), new Manager()};
+            List<IEat> workers2 = new List<IEat>() { new Worker() };
+        }
+    }
+}
---
+++ MultipleImplementation/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleImplementation
+{
+    internal class Worker : IWorker,ISalary,IEat
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

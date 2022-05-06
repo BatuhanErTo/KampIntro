3bf69b6c54ac21cba60af0cc03f25effc7a2caca
Batuhan Erol <bataer57@gmail.com>
2022-05-06 15:06:44 +0300

    Added | Encapsulation

---
+++ Encapsulation1/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation1
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+
+        private string _firstName;
+        public string FirstName { 
+            get
+            {
+                return "Mrs/Mrs." + _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName { get; set; }
+    }
+}
---
+++ Encapsulation1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Encapsulation1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Batuhan";
+            customer.LastName = "Erol";
+            customer.Id = 10;
+            Console.WriteLine(customer.FirstName);
+        }
+    }
+}

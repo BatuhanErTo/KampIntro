3652d1990f1b35bc93e8cf3db0abdcff0dde40d1
Batuhan Erol <bataer57@gmail.com>
2022-07-03 17:12:21 +0300

    Added | Assignment

---
+++ 6/SQL/Assignment.sql
@@ -0,0 +1,4 @@
+﻿Select Products.ProductName AS [Urun Adi], SUM([Order Details].UnitPrice * [Order Details].Quantity) AS [Kazanilan Toplam Miktar] From Products 
+INNER JOIN [Order Details] ON Products.ProductID = [Order Details].ProductID
+INNER JOIN Orders ON [Order Details].OrderID = Orders.OrderID
+GROUP BY Products.ProductName
